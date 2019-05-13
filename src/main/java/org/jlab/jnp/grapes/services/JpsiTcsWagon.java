cbe654fea749e663c8cfbde15196e8b24bc51d0b
baltzell <baltzell@gmx.com>
2019-05-13 16:25:04 -0400

    update jpsi/tcs wagon to hipo4, add abs(status)

--- src/main/java/org/jlab/jnp/grapes/services/JpsiTcsWagon.java
+++ src/main/java/org/jlab/jnp/grapes/services/JpsiTcsWagon.java
@@ -2,10 +2,13 @@ package org.jlab.jnp.grapes.services;
 
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import org.jlab.jnp.hipo4.data.Bank;
 import org.jlab.jnp.hipo4.data.Event;
 import org.jlab.jnp.hipo4.data.SchemaFactory;
+import java.util.ArrayList;
+import java.util.HashMap;
+//import org.jlab.jnp.hipo4.data.Event;
+//import org.jlab.jnp.hipo4.data.SchemaFactory;
 
 /**
  * 
@@ -17,13 +20,13 @@ import org.jlab.jnp.hipo4.data.SchemaFactory;
  */
 public class JpsiTcsWagon extends Wagon {
 
-    static final float muEcoutMax = 0.085f;
-    static final float muEcinMax = 0.055f;
-    static final float muPcalMax = 0.045f;
-    static final float momHigh = 2.0f;
+    static final float MIP_ECOUT_MAX = 0.085f;
+    static final float MIP_ECIN_MAX = 0.055f;
+    static final float MIP_PCAL_MAX = 0.045f;
+    static final float MOM_HIGH = 2.0f;
     
     public JpsiTcsWagon(){
-        super("JpsiTcsWagon","baltzell","0.1");
+        super("JpsiTcsWagon","baltzell","0.2");
     }
 
     @Override
@@ -32,63 +35,72 @@ public class JpsiTcsWagon extends Wagon {
         return true;
     }
 
+    private HashMap<Integer,ArrayList<Integer>> mapByIndex(Bank pindexBank) {
+        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
+        for (int ii=0; ii<pindexBank.getRows(); ii++) {
+            final int pindex = pindexBank.getInt("pindex",ii);
+            if (!map.containsKey(pindex)) map.put(pindex,new ArrayList<>());
+            map.get(pindex).add(ii);
+        }
+        return map;
+    }
+
     @Override
     public boolean processDataEvent(Event event, SchemaFactory factory) {
 
-       /* if (!event.hasGroup("REC::Particle") ||
-            !event.hasGroup("REC::Calorimeter")) return false;
+        Bank particles = new Bank(factory.getSchema("REC::Particle"));
+        Bank calorimeters = new Bank(factory.getSchema("REC::Calorimeter"));
 
-        // REC::Particle:
-        HipoNode pids    = event.getNode(331,1);
-        HipoNode charges = event.getNode(331,8);
-        HipoNode statii  = event.getNode(331,11);
-
-        // REC::Calorimeter:
-        HipoNode pindices = event.getNode(332,2);
-
-        // abort if either bank is empty:
-        if (pids==null || pindices==null || pids.getDataSize()<1 || pindices.getDataSize()<1) return false;
-
+        event.read(particles);
+        event.read(calorimeters);
+       
+        if (particles.getRows()<1) return false;
+        if (calorimeters.getRows()<1) return false;
+        
         // load map from REC::Particle rows to REC::Calorimeter rows:
-        HashMap<Integer,ArrayList<Integer>> part2calo = this.mapByIndex(pindices);
+        HashMap<Integer,ArrayList<Integer>> part2calo = this.mapByIndex(calorimeters);
         
         int npos=0,nposFD=0;
-        ArrayList<Integer> eleCandi = new ArrayList<Integer>();
-        ArrayList<Integer> posCandi = new ArrayList<Integer>();
-        ArrayList<Integer> mupCandi = new ArrayList<Integer>();
-        ArrayList<Integer> mumCandi = new ArrayList<Integer>();
+        ArrayList<Integer> eleCandi = new ArrayList<>();
+        ArrayList<Integer> posCandi = new ArrayList<>();
+        ArrayList<Integer> mupCandi = new ArrayList<>();
+        ArrayList<Integer> mumCandi = new ArrayList<>();
 
-        for (int ipart=0; ipart<pids.getDataSize(); ipart++) {
+        for (int ipart=0; ipart<particles.getRows(); ipart++) {
            
-            if (charges.getInt(ipart) == 0) continue;
+            final int pid=particles.getInt("pid",ipart);
+            final int charge=particles.getInt("charge",ipart);
+            final int status=particles.getInt("status",ipart);
+            
+            if (charge == 0) continue;
 
-            final boolean isFD = (int)(statii.getInt(ipart)/1000) == 2;
+            final boolean isFD = (int)(Math.abs(status)/1000) == 2;
 
             // count positives:
-            if (charges.getInt(ipart) > 0) {
+            if (charge > 0) {
                 if (isFD) nposFD++;
                 npos++;
             }
 
             // electron/positron candidates based on EB pid:
             if (isFD) {
-                if      (pids.getInt(ipart)== 11) eleCandi.add(ipart);
-                else if (pids.getInt(ipart)==-11) posCandi.add(ipart);
+                if      (pid== 11) eleCandi.add(ipart);
+                else if (pid==-11) posCandi.add(ipart);
             }
 
             // muon candidates based on EC energy:
             if (part2calo.containsKey(ipart)) {
-                ECAL ecal = new ECAL(part2calo.get(ipart),event);
+                ECAL ecal = new ECAL(part2calo.get(ipart),calorimeters);
                 if (ecal.isMIP()) {
-                    if (charges.getInt(ipart) > 0) mupCandi.add(ipart);
-                    else                           mumCandi.add(ipart);
+                    if (charge > 0) mupCandi.add(ipart);
+                    else            mumCandi.add(ipart);
                 }
             }
         }
 
         // abort asap:
-        if ( (eleCandi.size()==0 || posCandi.size()==0) &&
-             (mumCandi.size()==0 || mupCandi.size()==0) ) return false;
+        if ( (eleCandi.isEmpty() || posCandi.isEmpty()) &&
+             (mumCandi.isEmpty() || mupCandi.isEmpty()) ) return false;
 
         ///////////////////////////////////////////////////////////////////
         ///////////////////////////////////////////////////////////////////
@@ -100,53 +112,40 @@ public class JpsiTcsWagon extends Wagon {
         if (mumCandi.size()>0 && mupCandi.size()>0 && nposFD>1) return true;
 
         // e/mu candidates with "high" momentum:
-        ArrayList<Integer> eleHiCandi = new ArrayList<Integer>();
-        ArrayList<Integer> posHiCandi = new ArrayList<Integer>();
-        ArrayList<Integer> mupHiCandi = new ArrayList<Integer>();
-        ArrayList<Integer> mumHiCandi = new ArrayList<Integer>();
-        for (int ii : eleCandi) if (this.getMomentum(ii,event) > momHigh) eleHiCandi.add(ii);
-        for (int ii : posCandi) if (this.getMomentum(ii,event) > momHigh) posHiCandi.add(ii);
-        for (int ii : mumCandi) if (this.getMomentum(ii,event) > momHigh) mumHiCandi.add(ii);
-        for (int ii : mupCandi) if (this.getMomentum(ii,event) > momHigh) mupHiCandi.add(ii);
+        ArrayList<Integer> eleHiCandi = new ArrayList<>();
+        ArrayList<Integer> posHiCandi = new ArrayList<>();
+        ArrayList<Integer> mupHiCandi = new ArrayList<>();
+        ArrayList<Integer> mumHiCandi = new ArrayList<>();
+        for (int ii : eleCandi) if (this.getMomentum(ii,particles) > MOM_HIGH) eleHiCandi.add(ii);
+        for (int ii : posCandi) if (this.getMomentum(ii,particles) > MOM_HIGH) posHiCandi.add(ii);
+        for (int ii : mumCandi) if (this.getMomentum(ii,particles) > MOM_HIGH) mumHiCandi.add(ii);
+        for (int ii : mupCandi) if (this.getMomentum(ii,particles) > MOM_HIGH) mupHiCandi.add(ii);
 
         // J/Psi, high-momentum e+e-:
         if (eleHiCandi.size()>0 && posHiCandi.size()>0) return true;
 
         // J/Psi, high-momentum mu+mu-:
         if (mumHiCandi.size()>0 && mupHiCandi.size()>0) return true;
-        */
+
         return false;
     }
 
-    /*private HashMap<Integer,ArrayList<Integer>> mapByIndex(HipoNode indices) {
-        HashMap<Integer,ArrayList<Integer>> map=new HashMap<Integer,ArrayList<Integer>>();
-        for (int ii=0; ii<indices.getDataSize(); ii++) {
-            final int index = indices.getInt(ii);
-            if (!map.containsKey(index)) map.put(index,new ArrayList<Integer>());
-            map.get(index).add(ii);
-        }
-        return map;
-    }*/
-    /*
     private class ECAL {
         private float epcal=-1;
         private float eecin=-1;
         private float eecou=-1; 
-        public ECAL(ArrayList<Integer>icalos, HipoEvent event) {
-            HipoNode detectors = event.getNode(332,3);
-            HipoNode layers    = event.getNode(332,5);
-            HipoNode energies  = event.getNode(332,6);
+        public ECAL(ArrayList<Integer>icalos, Bank calorimeters) {
             for (int icalo : icalos) {
-                if (detectors.getInt(icalo) != 7) continue;
-                switch (layers.getByte(icalo)) {
+                if (calorimeters.getInt("detector",icalo) != 7) continue;
+                switch (calorimeters.getByte("layer",icalo)) {
                     case 1:
-                        epcal = energies.getFloat(icalo);
+                        epcal = calorimeters.getFloat("energy",icalo);
                         break;
                     case 4:
-                        eecin = energies.getFloat(icalo);
+                        eecin = calorimeters.getFloat("energy",icalo);
                         break;
                     case 7:
-                        eecou = energies.getFloat(icalo);
+                        eecou = calorimeters.getFloat("energy",icalo);
                         break;
                     default:
                         break;
@@ -155,18 +154,17 @@ public class JpsiTcsWagon extends Wagon {
         }
         public boolean isMIP() {
             if (this.epcal<0 || this.eecin<0 || this.eecou<0) return false;
-            if (this.epcal > muPcalMax) return false;
-            if (this.eecin > muEcinMax) return false;
-            if (this.eecou > muEcoutMax) return false;
+            if (this.epcal > MIP_PCAL_MAX) return false;
+            if (this.eecin > MIP_ECIN_MAX) return false;
+            if (this.eecou > MIP_ECOUT_MAX) return false;
             return true;
         }
     }
 
-    private float getMomentum(final int ipart, HipoEvent event) {
-        final float px = event.getNode(331,2).getFloat(ipart);
-        final float py = event.getNode(331,3).getFloat(ipart);
-        final float pz = event.getNode(331,4).getFloat(ipart);
+    private float getMomentum(final int ipart, Bank particles) {
+        final float px = particles.getFloat("px",ipart);
+        final float py = particles.getFloat("py",ipart);
+        final float pz = particles.getFloat("pz",ipart);
         return (float)Math.sqrt(px*px + py*py + pz*pz);
     }
-    */
 }
