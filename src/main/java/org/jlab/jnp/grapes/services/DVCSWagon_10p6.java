2ce951d006d4d1cd26eca090cb56f7a23c2773eb
Francois-Xavier Girod <fxgirod@jlab.org>
2020-06-13 17:34:52 -0400

    v0 progress

--- src/main/java/org/jlab/jnp/grapes/services/DVCSWagon_10p6.java
+++ src/main/java/org/jlab/jnp/grapes/services/DVCSWagon_10p6.java
@@ -1,17 +1,11 @@
 package org.jlab.jnp.grapes.services;
 
-import java.io.*;
-import java.util.*;
-
 import org.jlab.jnp.physics.Vector3;
 import org.jlab.jnp.physics.LorentzVector;
 
 import org.jlab.jnp.hipo4.data.Bank;
 import org.jlab.jnp.hipo4.data.Event;
 import org.jlab.jnp.hipo4.data.SchemaFactory;
-import org.jlab.jnp.hipo4.io.HipoReader;
-import org.jlab.jnp.hipo4.io.HipoWriter;
-
 
 /**
 * 
@@ -25,7 +19,7 @@ public class DVCSWagon_10p6 extends Wagon {
     static final double BeamEnergy = 10.603f;
     static final double TargetMass = 0.93827f;
 
-    public LorentzVector VB, VT, VE, VP, VG;
+    public LorentzVector VB, VT;
 
     public DVCSWagon_10p6() {
         super("DVCSWagon","fxgirod","0.0");
@@ -66,7 +60,7 @@ public class DVCSWagon_10p6 extends Wagon {
 
                             double e_mom = Math.sqrt(e_px*e_px+e_py*e_py+e_pz*e_pz);
                             if( is_e==11 && e_stat>2000 && e_stat<4000 && e_mom>0.1*BeamEnergy ){
-                                    VE = new LorentzVector(e_px,e_py,e_pz,e_mom);
+				    LorentzVector VE = new LorentzVector(e_px,e_py,e_pz,e_mom);
                                     for (int ip = ie+1; ip < RecPart.getRows()-1 && !hasDVCS; ip++) {
                                             int is_p     = RecPart.getInt("pid", ip);
                                             double p_px  = RecPart.getFloat("px", ip);
@@ -76,7 +70,7 @@ public class DVCSWagon_10p6 extends Wagon {
 
                                             if( is_p==2212 && p_stat>2000 ){
                                                     double p_ene = Math.sqrt(p_px*p_px+p_py*p_py+p_pz*p_pz+TargetMass*TargetMass);
-                                                    VP = new LorentzVector(p_px,p_py,p_pz,p_ene);
+                                                    LorentzVector VP = new LorentzVector(p_px,p_py,p_pz,p_ene);
                                                     for (int ig = ip+1; ig < RecPart.getRows() && !hasDVCS; ig++) {
                                                             int is_g     = RecPart.getInt("pid", ig);
                                                             double g_px  = RecPart.getFloat("px", ig);
@@ -86,7 +80,7 @@ public class DVCSWagon_10p6 extends Wagon {
 
                                                             double g_mom = Math.sqrt(g_px*g_px+g_py*g_py+g_pz*g_pz);
                                                             if( is_g==22 && g_stat<4000 && g_mom>0.15*BeamEnergy){
-                                                                    VG = new LorentzVector(g_px,g_py,g_pz,g_mom);
+                                                                    LorentzVector VG = new LorentzVector(g_px,g_py,g_pz,g_mom);
 
                                                                     LorentzVector Q = new LorentzVector(0,0,0,0);
                                                                     Q.add(VB);
