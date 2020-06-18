0ed1502b31472bb46d69a6d1875961294ec10962
Nick Markov <markov@ifarm1802.jlab.org>
2020-06-17 20:37:28 -0400

    Updated Kaon wagon

--- src/main/java/org/jlab/jnp/grapes/services/ElecFDKPlusWagon.java
+++ src/main/java/org/jlab/jnp/grapes/services/ElecFDKPlusWagon.java
@@ -1,8 +1,11 @@
 package org.jlab.jnp.grapes.services;
-
+import org.jlab.jnp.physics.Vector3;
+import org.jlab.jnp.physics.LorentzVector;
 import org.jlab.jnp.hipo4.data.*;
 import org.jlab.jnp.hipo.data.HipoNode;
-
+import org.jlab.jnp.hipo4.data.Bank;
+import org.jlab.jnp.hipo4.data.Event;
+import org.jlab.jnp.hipo4.data.SchemaFactory;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -11,19 +14,12 @@ import java.util.HashMap;
  * Skim for elec FD Kaon plus analysis.
  *
  */
-public class eFDKPlusWagon extends Wagon {
+public class ElecFDKPlusWagon extends BeamTargetWagon {
     
-    static final double KaonMass   = 0.493677f;
     public ElecFDKPlusWagon(){
 	super("ElecFDKPlusWagon","markov","0.1");
     }
     
-    
-    @Override
-    public boolean init(String jsonString) {
-	System.out.println("ElecFDKPlusWagon READY.");
-	return true;
-    }
     
     @Override
     public boolean processDataEvent(Event event, SchemaFactory factory) {
@@ -42,7 +38,7 @@ public class eFDKPlusWagon extends Wagon {
 	boolean flag_elec = false;
 	boolean flag_kp = false;
 	
-	for (int ii = 0; ii < bankREC.getRows(); ii++) {
+	for (int ii = 0; ii < RecPart.getRows(); ii++) {
 	    pid = RecPart.getInt("pid", ii);
 	    status = (short) Math.abs(RecPart.getShort("status", ii));
 	    chi2 = (float) RecPart.getFloat("chi2pid", ii);
@@ -58,12 +54,11 @@ public class eFDKPlusWagon extends Wagon {
 		LorentzVector W = new LorentzVector(0,0,0,0);
 		W.add(Q);
 		W.add(VT);
-		if (W.Mass() > 1.3 && W.Mass() < 3){
+		if (W.mass() > 1.3 && W.mass() < 3.2){
 		    flag_elec = true;
 		}
-
 	    }
-	    if (pid == 321 && Math.abs(chi2pid) < 10)
+	    if (pid == 321 && Math.abs(chi2) < 10)
 		flag_kp = true;	    
 	}
 	
