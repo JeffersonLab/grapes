a03cdf5516c859b17acdf19409cc345b01c33996
gavalian <gavalian@meson.jlab.org>
2020-07-21 14:25:22 -0400

    change the version to 2.6

--- distribute.sh
+++ distribute.sh
@@ -2,7 +2,7 @@
 #=========================================
 # THIS IS LOCAL DISTRIBUTION SCRIPT
 #=========================================
-VERSION=2.5
+VERSION=2.6
 rm -rf grapes-$VERSION grapes-$VERSION.tar.gz
 mkdir -p grapes-$VERSION/lib/core
 mkdir -p grapes-$VERSION/lib/services
@@ -14,5 +14,5 @@ cp ../jnp/jnp-jaw/target/jnp-jaw-2.0-SNAPSHOT-jar-with-dependencies.jar grapes-$
 cp config/train.yaml grapes-$VERSION/config/.
 tar -cf grapes-$VERSION.tar grapes-$VERSION
 gzip grapes-$VERSION.tar
-scp grapes-$VERSION.tar.gz clas12@ifarm1401:/group/clas/www/clasweb/html/clas12offline/distribution/grapes/
+scp grapes-$VERSION.tar.gz clas12@ifarm1801:/group/clas/www/clasweb/html/clas12offline/distribution/grapes/
 #rm -rf grapes-$VERSION grapes-$VERSION.tar.gz
