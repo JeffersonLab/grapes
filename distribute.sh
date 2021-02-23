ae3e0724df10f6913e5a3b6f3762a1e8492058e5
gavalian <gavalian@meson.jlab.org>
2021-02-23 15:46:53 -0500

    changed the version of JAW jar to bring over

--- distribute.sh
+++ distribute.sh
@@ -2,7 +2,7 @@
 #=========================================
 # THIS IS LOCAL DISTRIBUTION SCRIPT
 #=========================================
-VERSION=2.7
+VERSION=2.8
 rm -rf grapes-$VERSION grapes-$VERSION.tar.gz
 mkdir -p grapes-$VERSION/lib/core
 mkdir -p grapes-$VERSION/lib/services
@@ -10,9 +10,9 @@ mkdir -p grapes-$VERSION/config
 cp -r bin grapes-$VERSION/.
 cp target/grapes-$VERSION-SNAPSHOT.jar grapes-$VERSION/lib/core/
 #cp $PROJECT/Distribution/jnp/jnp-distro/jaw-0.9/lib/jaw-0.9.jar grapes-$VERSION/lib/core/
-cp ../jnp/jnp-jaw/target/jnp-jaw-2.0-SNAPSHOT-jar-with-dependencies.jar grapes-$VERSION/lib/core/jaw-2.0.jar
+cp ../jnp/jnp-jaw/target/jnp-jaw-2.1-SNAPSHOT-jar-with-dependencies.jar grapes-$VERSION/lib/core/jaw-2.0.jar
 cp config/train.yaml grapes-$VERSION/config/.
 tar -cf grapes-$VERSION.tar grapes-$VERSION
 gzip grapes-$VERSION.tar
-scp grapes-$VERSION.tar.gz clas12@ifarm1801:/group/clas/www/clasweb/html/clas12offline/distribution/grapes/
+scp grapes-$VERSION.tar.gz clas12@ifarm1901:/group/clas/www/clasweb/html/clas12offline/distribution/grapes/
 #rm -rf grapes-$VERSION grapes-$VERSION.tar.gz
