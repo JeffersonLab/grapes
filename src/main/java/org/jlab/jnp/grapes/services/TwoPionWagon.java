7fbd82aeb141233e1ba3ad9de7d4f739a9724ccc
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2021-08-18 09:03:14 -0400

    Reorganize and comment twopiwagon

--- src/main/java/org/jlab/jnp/grapes/services/TwoPionWagon.java
+++ src/main/java/org/jlab/jnp/grapes/services/TwoPionWagon.java
@@ -10,13 +10,14 @@ import java.util.HashMap;
  * 
  * Skim for 2pion channel with e- in FD
  *
- * @author devita
+ * @author kneupane
+ * @author tylern
  */
 
 public class TwoPionWagon extends Wagon {
 
 	public TwoPionWagon(){
-        super("TwoPionWagon","devita","0.1");
+        super("TwoPionWagon","kneupane","0.1");
     }
 
 	@Override
@@ -28,53 +29,57 @@ public class TwoPionWagon extends Wagon {
 	@Override
 	public boolean processDataEvent(Event event, SchemaFactory factory) {
 
-//		Bank bank = new Bank(factory.getSchema("REC::Particle"));
 		Bank bankREC   = new Bank(factory.getSchema("REC::Particle"));
-
-//		event.read(bank);
-
 		event.read(bankREC);
 
-		boolean flag_TwoPion = false;
-		int     nelec  = 0;
-                int     nprot = 0;
-                int     npip = 0;
-                int     npim = 0;
+        // If the bank is null or empty don't save the event
+        if(bankREC == null) return false;
 
+        int     nelec  = 0;
+        int     nprot = 0;
+        int     npip = 0;
+        int     npim = 0;
+        int     nother = 0;
+
+        for (int ii = 0; ii < bankREC.getRows(); ii++) {
+            int pid    = bankREC.getInt("pid", ii);
+            double  px = bankREC.getFloat("px", ii);
+            double  py = bankREC.getFloat("py", ii);
+            double  pz = bankREC.getFloat("pz", ii);
+            double   p = Math.sqrt(px*px+py*py+pz*pz);
+            int status = bankREC.getShort("status", ii);
+
+            // Electron in forward detector (status between 2000 and 4000) and not low momentum (p < 1)
+            if (pid == 11 && 2000 >= Math.abs(status) && Math.abs(status) < 4000 && p>1) nelec++;
+
+            // Particle in forward or central detector
+            if(status>=2000) {
+                // Count based on PIDs
+                if (pid == 2212)    nprot++;
+                else if(pid == 211) npip++;
+                else if(pid == -211) npim++;
+                else nother++;
+            }
+        }
+
+        // Checks on the event
+        boolean     hasElectron = nelec>=1;
+
+        // TODO: Look into seeing if we need to change this or include nother in count
+        boolean     notTooMany = ((nprot+npip+npim)<=7);
+
+        // Setup each topology
+        boolean     exclusive = (nprot >=1 && npip >=1 && npim >=1);
+        boolean     missingPim = (nprot >=1 && npip >=1);
+        boolean     missingPip = (nprot >=1 && npim >=1);
+        boolean     missingProt = (npip >=1 && npim >=1);
+        
+
+        // If we have an electron and not too many other particles 
+        // and the event fits one of the topologies save the event
+        boolean goodTwoPion = (hasElectron && notTooMany && (exclusive || missingPim || missingPip || missingProt));  
                 
-                if( bankREC!=null) {
-                    for (int ii = 0; ii < bankREC.getRows(); ii++) {
-                            int pid    = bankREC.getInt("pid", ii);
-                            int charge = bankREC.getByte("charge", ii);
-                            double  px = bankREC.getFloat("px", ii);
-                            double  py = bankREC.getFloat("py", ii);
-                            double  pz = bankREC.getFloat("pz", ii);
-                            double   p = Math.sqrt(px*px+py*py+pz*pz);
-                            int status = bankREC.getShort("status", ii);
-
-                            if (pid == 11 && (2000 <= Math.abs(status) < 4000) && p>1) nelec++; 
-                            if(status>=2000) {
-                                if (pid == 2212 && charge >0 )    nprot++;
-                                else if(pid == 211 &&  charge>0) npip++;
-                                else if(pid == -211 &&  charge<0) npim++;
-                            }
-                    }
-
-                    if (nelec>=1 && ((nprot+npip)>=2 || (nprot+npim)>=2 || (npip+npim)>=2 || (nprot+npip+npim)>=3)) && (nprot+npip+npim)<=7) flag_TwoPion=true;
-                }
-                
-                return flag_TwoPion;
-	}
-
-	private HashMap<Integer, ArrayList<Integer>> mapByIndex(HipoNode indices) {
-		HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
-		for (int ii = 0; ii < indices.getDataSize(); ii++) {
-			final int index = indices.getInt(ii);
-			if (!map.containsKey(index))
-				map.put(index, new ArrayList<Integer>());
-			map.get(index).add(ii);
-		}
-		return map;
+        return goodTwoPion;
 	}
 
 }
