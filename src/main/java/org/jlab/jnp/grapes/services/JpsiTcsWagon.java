c1b62142aa28a42c7a02a2686375a800ad57bba2
Nathan Baltzell <baltzell@baltzellmac.jlab.org>
2023-05-01 14:36:45 -0400

    account for ft electron trigger row

--- src/main/java/org/jlab/jnp/grapes/services/JpsiTcsWagon.java
+++ src/main/java/org/jlab/jnp/grapes/services/JpsiTcsWagon.java
@@ -48,16 +48,22 @@ public class JpsiTcsWagon extends BeamTargetWagon {
     public boolean isMissingAndTagged(Event event, SchemaFactory factory) {
         Bank particles = new Bank(factory.getSchema("RECFT::Particle"));
         event.read(particles);
-        if (particles.getRows()<2) return false;
-        if (particles.getInt("pid",0) != 11) return false;
-        if ( (int) (Math.abs(particles.getInt("status",0))/1000) != 1) return false;
-        LorentzVector electron = Util.getLorentzVector(particles, 0);
-        for (int i=1; i<particles.getRows(); ++i) {
-            if (particles.getInt("pid",i) == 2212) {
-                LorentzVector proton = Util.getLorentzVector(particles, i);
-                if (getMissingVector(proton,electron).mass() > MIN_TAGGED_MISS_MASS) {
-                    return true;
-                }
+        final int nrows = particles.getRows();
+        int ielec = -1;
+        for (int i=0; i<nrows; ++i) {
+            if (particles.getInt("pid",i) != 11) continue;
+            if (particles.getInt("status",i)/1000 != -1) continue;
+            ielec = i;
+            break;
+        }
+        if (ielec < 0) return false;
+        LorentzVector electron = Util.getLorentzVector(particles, ielec);
+        for (int i=0; i<nrows; ++i) {
+            if (particles.getInt("pid",i) != 2212) continue;
+            if (particles.getInt("status",i)/2000 != 1) continue;
+            LorentzVector proton = Util.getLorentzVector(particles, i);
+            if (getMissingVector(proton,electron).mass() > MIN_TAGGED_MISS_MASS) {
+                return true;
             }
         }
         return false;
