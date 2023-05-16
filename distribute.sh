60132925df4a9e8f4415e135f3f74d80725355aa
gavalian <gavalian@jlab.org>
2023-05-16 11:06:25 -0400

    modified the pom to produce the uber jar, and script to properly pack the distribution

--- distribute.sh
+++ distribute.sh
@@ -2,17 +2,30 @@
 #=========================================
 # THIS IS LOCAL DISTRIBUTION SCRIPT
 #=========================================
-VERSION=2.8
+VERSION=2.14
+#===================================================
+# Remove previous distribution
+#===================================================
 rm -rf grapes-$VERSION grapes-$VERSION.tar.gz
+#===================================================
+# create directory and copy config and bin files
+#===================================================
 mkdir -p grapes-$VERSION/lib/core
 mkdir -p grapes-$VERSION/lib/services
 mkdir -p grapes-$VERSION/config
 cp -r bin grapes-$VERSION/.
-cp target/grapes-$VERSION-SNAPSHOT.jar grapes-$VERSION/lib/core/
-#cp $PROJECT/Distribution/jnp/jnp-distro/jaw-0.9/lib/jaw-0.9.jar grapes-$VERSION/lib/core/
-cp ../jnp/jnp-jaw/target/jnp-jaw-2.1-SNAPSHOT-jar-with-dependencies.jar grapes-$VERSION/lib/core/jaw-2.0.jar
 cp config/train.yaml grapes-$VERSION/config/.
+#===================================================
+# copy JAR to lib directory
+#===================================================
+cp target/grapes-$VERSION-core.jar grapes-$VERSION/lib/core/
+#===================================================
+# create tarball for the package and copy to userweb
+# web space with clas12 privileges.
+#============================================
 tar -cf grapes-$VERSION.tar grapes-$VERSION
 gzip grapes-$VERSION.tar
 scp grapes-$VERSION.tar.gz clas12@ifarm1901:/group/clas/www/clasweb/html/clas12offline/distribution/grapes/
-#rm -rf grapes-$VERSION grapes-$VERSION.tar.gz
+#============================================
+# THE END OF THE SCRIPT
+#============================================
